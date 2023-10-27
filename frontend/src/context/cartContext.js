b4de8fd2f7ac0ba77860fe6a6e307ce007079d35
Yosef David Giraldo Salazar <yosefdavid91@gmail.com>
2023-10-26 22:30:46 -0500

    update cartContext

--- frontend/src/context/cartContext.js
+++ frontend/src/context/cartContext.js
@@ -4,6 +4,31 @@ const CartContext = createContext();
 const CartProvider = ({children}) =>{
     const[cart, setCart] = useState([]);
 
+    const emptyCart = () => setCart([])
+
+
+    const findProdInCart = (product) =>{
+        return cart.findIndex(cartItem => cartItem.id === product.id)
+    }
+
+    const deleteProduct = (product) =>{
+        const indexItem = findProdInCart(product)
+        if (indexItem !== -1){
+            cart.splice(indexItem, 1) 
+        }
+    }
+
+    const addProduct = (product) =>{
+        const indexItem = findProdInCart(product)
+        if (indexItem !== -1){
+            
+        }else{
+            cart.push({...product})
+        }
+    }
+
+
+
     useEffect(()=>{
         let currentCart = localStorage.getItem("cart")
         if(currentCart) setCart(JSON.parse(currentCart))
